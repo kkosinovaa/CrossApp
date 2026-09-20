4857b1a5757682c697cdcfd4371895159733f839
kkosinovaa <kkatrusaptasechka@gmail.com>
2026-09-20 15:40:26 +0300

    lab2 + additional task

--- src/Core/EnvironmentInfo.cs
+++ src/Core/EnvironmentInfo.cs
@@ -1,27 +1,37 @@
 ﻿using System.Runtime.InteropServices;
-namespace Core; 
+namespace Core;
+
 public sealed record EnvironmentReport(
-string OsDescription,
-string FrameworkDescription,
-string ProcessArchitecture,
-string DetectedRid,
-string ReportedRid,
-string BaseDirectory);
+    string OsDescription,
+    string FrameworkDescription,
+    string ProcessArchitecture,
+    string DetectedRid,
+    string ReportedRid,
+    string BaseDirectory,
+    string BuildNote); 
+
 public static class EnvironmentInfo
 {
+#if NET10_0_OR_GREATER
+    const string BuildNote = "збірка під net10.0";
+#else
+    const string BuildNote = "збірка під net8.0";
+#endif
+
     public static EnvironmentReport Collect() => new(
-    RuntimeInformation.OSDescription,
-    RuntimeInformation.FrameworkDescription,
-    RuntimeInformation.ProcessArchitecture.ToString(),
-    DetectRid(),
-    RuntimeInformation.RuntimeIdentifier,
-    AppContext.BaseDirectory);
+        RuntimeInformation.OSDescription,
+        RuntimeInformation.FrameworkDescription,
+        RuntimeInformation.ProcessArchitecture.ToString(),
+        DetectRid(),
+        RuntimeInformation.RuntimeIdentifier,
+        AppContext.BaseDirectory,
+        BuildNote); 
+
     private static string DetectRid()
     {
-        string os =
-        RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" :
-        RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "linux" :
-        RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "unknown";
+        string os = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "win" :
+                    RuntimeInformation.IsOSPlatform(OSPlatform.Linux) ? "linux" :
+                    RuntimeInformation.IsOSPlatform(OSPlatform.OSX) ? "osx" : "unknown";
         string arch = RuntimeInformation.ProcessArchitecture switch
         {
             Architecture.X64 => "x64",
